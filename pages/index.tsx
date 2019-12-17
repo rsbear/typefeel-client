b8f4e1f95c75262f3fdb7151887611f271cf8f3a
rsbear <hellorosss@gmail.com>
2019-12-17 15:11:18 -0600

    Adding content to the landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,106 @@
 import React, { FC } from "react";
 import Layout from "components/layouts/Layout";
 import { GetProps } from "interfaces/GetProps";
+import Link from "next/link";
+import { text } from "styles/text";
+import { flex, grid50, colors } from "styles/main";
+import { css } from "@emotion/core";
+import { useSortKeyboardsQuery, useSortKeysetsQuery } from "generated/graphql";
+import Tile from "components/Tile";
 
 const Landing: GetProps<any> = ({ authUser }) => {
+  const keyboards = useSortKeyboardsQuery({
+    variables: { where: { interestCheck: true } }
+  });
+  const keysets = useSortKeysetsQuery({
+    variables: { where: { interestCheck: true } }
+  });
   return (
     <Layout title="Home" authUser={authUser}>
-      <h2>landing page</h2>
-      {/* {test.data.Keyboard.map((a: any) => (
-        <div key={a.id}>
-          <h2>{a.size}</h2>
-          <h2>{a.name}</h2>
+      {!authUser && (
+        <div css={[flex.row, grid50]}>
+          <div css={linkBox}>
+            <Link href="/login">
+              <a>
+                <h1>Log in</h1>
+                <p>To do stuff..</p>
+              </a>
+            </Link>
+          </div>
+          <div css={linkBox}>
+            <Link href="/signup">
+              <a>
+                <h1>Sign up</h1>
+                <p>So you can log in...</p>
+              </a>
+            </Link>
+          </div>
         </div>
-      ))} */}
+      )}
+      {keyboards.loading || (keysets.loading && <h2>Loading</h2>)}
+      <h1>Keyboards in interest check</h1>
+      {!keyboards.loading &&
+        !keyboards.error &&
+        keyboards.data &&
+        keyboards.data.sortKeyboards && (
+          <div css={grid50}>
+            {keyboards.data.sortKeyboards.map((k: any) => (
+              <Link
+                href="/keyboard/[shortId]"
+                as={`/keyboard/${k.shortId}`}
+                key={k.id}
+              >
+                <a>
+                  <Tile
+                    image={k.images600[0]}
+                    mount={k.mount}
+                    name={k.name}
+                    size={k.size}
+                    key={k.id}
+                  />
+                </a>
+              </Link>
+            ))}
+          </div>
+        )}
+      {keysets.loading && <h2>Loading</h2>}
+      <h1>Keysets in interest check</h1>
+      {!keysets.loading &&
+        !keysets.error &&
+        keysets.data &&
+        keysets.data.sortKeysets && (
+          <div css={grid50}>
+            {keysets.data.sortKeysets.map((k: any) => (
+              <Link
+                href="/keyset/[shortId]"
+                as={`/keyset/${k.shortId}`}
+                key={k.id}
+              >
+                <a>
+                  <Tile
+                    image={k.images600[0]}
+                    mount={null}
+                    name={k.name}
+                    size={k.profile}
+                    key={k.id}
+                  />
+                </a>
+              </Link>
+            ))}
+          </div>
+        )}
     </Layout>
   );
 };
 
 export default Landing;
+
+const linkBox = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 15px;
+  border: solid 1px ${colors.black05};
+  border-radius: 4px;
+`;
